1af7cddc0c66dc3bdce8833d70e745cc58f475d3
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-03-04 00:10:31 +0800

    refactor: remove the scoreTable, storage score directly

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -3,16 +3,15 @@ package cn.xpbootcamp.tennis.game1;
 import cn.xpbootcamp.tennis.TennisGame;
 import com.google.common.collect.ImmutableMap;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
 
-    public static final int DEFAULT_SCORE = 0;
-
     private String firstPlayerName;
     private String secondPlayerName;
-    private Map<String, Integer> scoreTable = new HashMap<>();
+    private int firstPlayerScore;
+    private int secondPlayerScore;
+
     private Map<Integer, String> scoreMap = ImmutableMap.<Integer, String>builder()
             .put(0, "Love")
             .put(1, "Fifteen")
@@ -23,20 +22,17 @@ public class TennisGame1 implements TennisGame {
     public TennisGame1(String firstPlayerName, String secondPlayerName) {
         this.firstPlayerName = firstPlayerName;
         this.secondPlayerName = secondPlayerName;
-        scoreTable.put(firstPlayerName, DEFAULT_SCORE);
-        scoreTable.put(secondPlayerName, DEFAULT_SCORE);
     }
 
     public void wonPoint(String playerName) {
-        if (scoreTable.containsKey(playerName)) {
-            scoreTable.put(playerName, scoreTable.get(playerName) + 1);
+        if (firstPlayerName.equals(playerName)) {
+            firstPlayerScore++;
+        } else {
+            secondPlayerScore++;
         }
     }
 
     public String getScore() {
-        int firstPlayerScore = scoreTable.get(firstPlayerName);
-        int secondPlayerScore = scoreTable.get(secondPlayerName);
-
         if (isTwoScoreEquals()) {
             return getScoreWhenEqual();
         }
@@ -59,31 +55,25 @@ public class TennisGame1 implements TennisGame {
         return scoreResult;
     }
 
-    private String getScoreWhenOverThree() {
-        int firstPlayerScore = scoreTable.get(firstPlayerName);
-        int secondPlayerScore = scoreTable.get(secondPlayerName);
-
-        int scoreGap = firstPlayerScore - secondPlayerScore;
-        String scorePrefix = Math.abs(scoreGap) == 1 ? "Advantage " : "Win for ";
-        String scorePlayer = scoreGap > 0 ? firstPlayerName : secondPlayerName;
-        return scorePrefix + scorePlayer;
-    }
-
-    private boolean isAnyScoreOverThree() {
-        int firstPlayerScore = scoreTable.get(firstPlayerName);
-        int secondPlayerScore = scoreTable.get(secondPlayerName);
-        return firstPlayerScore >= 4 || secondPlayerScore >= 4;
-    }
-
     private boolean isTwoScoreEquals() {
-        return scoreTable.get(firstPlayerName).equals(scoreTable.get(secondPlayerName));
+        return firstPlayerScore == secondPlayerScore;
     }
 
     private String getScoreWhenEqual() {
-        int firstPlayerScore = scoreTable.get(firstPlayerName);
         if (firstPlayerScore <= 2) {
             return scoreMap.get(firstPlayerScore) + "-All";
         }
         return "Deuce";
+    }
+
+    private boolean isAnyScoreOverThree() {
+        return firstPlayerScore >= 4 || secondPlayerScore >= 4;
+    }
+
+    private String getScoreWhenOverThree() {
+        int scoreGap = firstPlayerScore - secondPlayerScore;
+        String scorePrefix = Math.abs(scoreGap) == 1 ? "Advantage " : "Win for ";
+        String scorePlayer = scoreGap > 0 ? firstPlayerName : secondPlayerName;
+        return scorePrefix + scorePlayer;
     }
 }
