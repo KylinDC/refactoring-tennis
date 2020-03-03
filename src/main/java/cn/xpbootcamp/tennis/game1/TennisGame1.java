1e96ec82b89e2373b32b6c4c3fd95e837513cdd6
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-03-03 20:17:40 +0800

    refactor: simple clean up

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -6,16 +6,16 @@ public class TennisGame1 implements TennisGame {
 
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private String firstPlayerName;
+    private String secondPlayerName;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String firstPlayerName, String secondPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals(firstPlayerName))
             m_score1 += 1;
         else
             m_score2 += 1;
